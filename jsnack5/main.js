163bb027a7abd98d32a16673817f8ca24c376af2
PaoloNapoli97 <paolonapoli@live.it>
2022-10-26 12:37:12 +0200

    jsnack-5

--- jsnack5/main.js
+++ jsnack5/main.js
@@ -2,7 +2,7 @@ const array = []
 
 for (let i = 0; i < 6; i++) {
     const numero = Number(prompt('Inserisci un numero'));
-    console.log("Numeri Inseriti: ",3 numero);
+    console.log("Numeri Inseriti: ", numero);
     if(numero % 2 == 1){
         array.push(numero);
     }
