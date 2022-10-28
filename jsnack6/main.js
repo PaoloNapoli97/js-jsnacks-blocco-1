6b626199c67751516c43a64812881d9d14401911
PaoloNapoli97 <paolonapoli@live.it>
2022-10-28 10:32:13 +0200

    jsnack6

--- jsnack6/main.js
+++ jsnack6/main.js
@@ -1,11 +1,11 @@
 "use strict";
 
-const array = ["1", "2", "3", "4", "5", "6"];
-const somma = 0;
+const array = [1, 2, 3, 4, 5, 6];
+let somma = 0;
 
 for (let i = 0; i < array.length; i++) {
     if(i % 2 == 1){
-        somma = array[i]
+        somma = somma + array[i];
     }
 }
 
